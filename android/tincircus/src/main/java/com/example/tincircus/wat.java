46752c41b605deafca95005c4d9165a8bc55fd47
StevieCode <sdcowan@hotmail.com>
2019-02-09 11:19:57 +0000

    9th feb safe update

---
+++ android/tincircus/src/main/java/com/example/tincircus/wat.java
@@ -0,0 +1,13 @@
+package com.example.tincircus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class wat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wat);
+    }
+}
